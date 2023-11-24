3e8ab0fe47066988a11a68fba1ae397bcadcd223
Ruken Afşin <rukenafsin13@gmail.com>
2023-11-24 22:32:02 +0300

    BlogRating Added and Parameterized Trigger Usage

--- DataAccessLayer/Migrations/20231124185852_mig_ratingtable.Designer.cs
+++ DataAccessLayer/Migrations/20231124185852_mig_ratingtable.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231124185852_mig_ratingtable")]
+    partial class mig_ratingtable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
