4244a7bc36ab629d9fcdfa297c950475339ab77f
Ruken Afşin <rukenafsin13@gmail.com>
2023-11-25 23:26:23 +0300

    Dashboard/Index Changes

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
@@ -11,6 +13,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1=c.Blogs.Count().ToString();
+            ViewBag.v2=c.Blogs.Where(x=>x.WriterID==1).Count().ToString();
+            ViewBag.v3=c.Categories.Count().ToString();
             return View();
         }
     }
